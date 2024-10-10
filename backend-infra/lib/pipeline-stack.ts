7edf3c5416a3248551737ee64731f01ef2ea05d0
Mayssen Aidoudi <mayssen.aidoudi@insat.ucar.tn>
2024-10-10 01:00:43 +0100

    some changes

--- backend-infra/lib/pipeline-stack.ts
+++ backend-infra/lib/pipeline-stack.ts
@@ -7,20 +7,29 @@ export class PipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    // Retrieve the GitHub token from the environment variable
+    const githubToken = process.env.GITHUB_TOKEN;
+
+    if (!githubToken) {
+      throw new Error("GITHUB_TOKEN environment variable is not set");
+    }
+
     const pipeline = new pipelines.CodePipeline(this, 'Pipeline', {
       pipelineName: 'MyBackendPipeline',
       synth: new pipelines.ShellStep('Synth', {
-        input: pipelines.CodePipelineSource.gitHub('MayseenAidoudi/Apply-AI', 'main'),
-        commands: ['cd backend-infra','npm ci', 'npm run build', 'npx cdk synth'],
-        primaryOutputDirectory: 'backend-infra/cdk.out'
+        input: pipelines.CodePipelineSource.gitHub('MayseenAidoudi/Apply-AI', 'main', {
+          authentication: cdk.SecretValue.plainText(githubToken) // Use the token directly
+        }),
+        commands: ['cd backend-infra', 'npm ci', 'npm run build', 'npx cdk synth'],
+        primaryOutputDirectory: 'backend-infra/cdk.out',
       }),
     });
-    const testingStage =  pipeline.addStage(new MyBackendStage(this, 'Test', {
+
+    const testingStage = pipeline.addStage(new MyBackendStage(this, 'Test', {
       env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
     }));
 
-    testingStage.addPost(new pipelines.ManualApprovalStep('manual approval before production'))
-
+    testingStage.addPost(new pipelines.ManualApprovalStep('manual approval before production'));
 
     pipeline.addStage(new MyBackendStage(this, 'Prod', {
       env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
